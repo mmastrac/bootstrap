deb033e332ee507a3724deb73dfe9275808f6cfe
Matt Mastracci <matthew@mastracci.com>
2018-07-07 12:59:20 -0600

    Starting work on lex_io test

--- bootstrap/bootstrap4/lex/tests/lex_io_test.s
+++ bootstrap/bootstrap4/lex/tests/lex_io_test.s
@@ -5,22 +5,24 @@
 	dd :_lex_io_test_create, &"test_create"
 	dd 0, 0
 
-:_lex_io_include_path
-	dd &"bootstrap/bootstrap4/lex_io_test"
-
 :_lex_io_test_create
 	%local ll
 	%local node
+	%local lex
 
 # Create the include list
 	%call :_ll_init
 	mov @ll, @ret
 	%call :_ll_create_node, 4
 	mov @node, @ret
-	st.w [@node], :_lex_io_include_path
+	st.w [@node], &"bootstrap/bootstrap4/lex/tests/lex_io_test"
 	%call :_ll_insert_head, @ll, @node
 
 # Create the lex environment
 	%call :__lex_create, @ll
+	mov @lex, @ret
+
+# Open a file
+	%call :__lex_open, @lex, &"bootstrap/bootstrap4/lex/tests/lex_io_test/test.c"
 
 	%ret
