30f92a5bff76504f0aeae7a4bef5e9d30b1e1c94
rain1 <rain1@airmail.cc>
2018-09-05 17:51:33 +0100

    * make.sh: Move the build script to /. Enable verbose script execution and fail on first error with. Use a local directory for build products. Validate the build products using sha256 checksums. * checksums.txt: created with sha256sum out/* after performing a build.

--- bootstrap/make.sh
+++
@@ -1,4 +0,0 @@
-cc vm.c -std=c99 -D_ATFILE_SOURCE=1 -o vm
-./vm bootstrap/bootstrap0.bin bootstrap/bootstrap1.s /tmp/b1.bin
-./vm /tmp/b1.bin bootstrap/bootstrap2.s /tmp/b2.bin
-./vm /tmp/b2.bin bootstrap/bootstrap3.s /tmp/b3.bin
---
+++ make.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -x
+set -e
+
+# Delete build products
+mkdir -p out/
+rm -f out/b1.bin out/b2.bin out/b3.bin
+
+# Perform the build
+cc vm.c -std=c99 -D_ATFILE_SOURCE=1 -o vm
+./vm bootstrap/bootstrap0.bin bootstrap/bootstrap1.s out/b1.bin
+./vm /tmp/b1.bin bootstrap/bootstrap2.s out/b2.bin
+./vm /tmp/b2.bin bootstrap/bootstrap3.s out/b3.bin
+
+# Verify correctness of build products
+sha256sum -c checksums.txt
