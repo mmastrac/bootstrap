08104b7061990b4bb0c33746a88e495a509f66db
Matt Mastracci <matthew@mastracci.com>
2022-05-07 05:53:23 -0600

    Test a local as well as an inline constant literal

--- bootstrap/bootstrap4/compiler0/tests/test_string_literal.c
+++ bootstrap/bootstrap4/compiler0/tests/test_string_literal.c
@@ -1,4 +1,6 @@
 int result_string_literal() {
+    const char* s;
+    s = "hello world";
     // Currently not doing the C ABI naming right here
-    return (_strlen("hello world") * 4) - 2;
+    return ((_strlen(s) + _strlen("hello world")) * 2) - 2;
 }
